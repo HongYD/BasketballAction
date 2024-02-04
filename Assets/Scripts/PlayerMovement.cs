f4c0449f2f00cb3e46625cbc34ab5f66b9396446
Dennis <nirvanagun93@gmail.com>
2024-02-04 21:23:57 +0800

    update: movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    InputActionsAsset playerMovement;
+    Vector2 move;
+    public float speed = 10;
+
+    private void Awake()
     {
-        
+        playerMovement = new InputActionsAsset();
+        playerMovement.PlayerControls.Move.performed += OnMove;
+        playerMovement.PlayerControls.Move.canceled += context=>move = Vector2.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMove(InputAction.CallbackContext context)
     {
-        
+        move = context.ReadValue<Vector2>();
+        Debug.Log($"按下方向键{move}" + context);
+    }
+
+    private void OnEnable()
+    {
+        playerMovement.PlayerControls.Enable();
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(move.x, 0.0f, move.y) * speed * Time.deltaTime;
+        transform.Translate(movement,Space.World);
+    }
+
+    private void OnDisable()
+    {
+        playerMovement.PlayerControls.Disable();
     }
 }
