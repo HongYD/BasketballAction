6821b161401d9598bae9059adf8ef8b33a169afa
Dennis <nirvanagun93@gmail.com>
2024-02-06 23:25:22 +0800

    update playerMovement/basketball player

--- Assets/Scripts/Player/BasketballPlayer.cs
+++ Assets/Scripts/Player/BasketballPlayer.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    Idle=0,
+    Move,
+    Shoot,
+    Pass,
+    Rececive,
+}
+
 public class BasketballPlayer : PlayerAgent
 {
+    [SerializeField]
+    private PlayerState state;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = PlayerState.Idle
     }
 
     // Update is called once per frame
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,96 +4,96 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public enum PlayerSpeedLevel
+public struct PlayerSpeedLevel
 {
-    Idel,
-    Jog,
-    Normal,
+    public const float Idle = 0;
+    public const float Jog = 1.0f;
+    public const float Nomal = 3.0f;
 }
 
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement/* : MonoBehaviour*/
 {
-    InputActionsAsset playerMovement;
+    //InputActionsAsset playerMovement;
     
     Vector2 move;
-    public float speed = 5;
+    //public float speed = 5;
 
-    Animator playerAnimator;
-    Animator ballAnimator;
+    //Animator playerAnimator;
+    //Animator ballAnimator;
 
 
-    private void Awake()
-    {
-        playerAnimator = this.GetComponent<Animator>();
-        ballAnimator = this.transform.Find("Ball").GetComponent<Animator>();
-        playerMovement = new InputActionsAsset();
-        playerMovement.PlayerControls.Move.performed += OnMove;
-        playerMovement.PlayerControls.Move.canceled += OnMoveCanceled;
-        playerMovement.PlayerControls.Shoot.performed += OnShoot;
-        playerMovement.PlayerControls.Pass.performed += OnPass;
-        playerMovement.PlayerControls.Receive.performed += OnReceive;
-    }
+    //private void Awake()
+    //{
+    //    playerAnimator = this.GetComponent<Animator>();
+    //    ballAnimator = this.transform.Find("Ball").GetComponent<Animator>();
+    //    playerMovement = new InputActionsAsset();
+    //    playerMovement.PlayerControls.Move.performed += OnMove;
+    //    playerMovement.PlayerControls.Move.canceled += OnMoveCanceled;
+    //    playerMovement.PlayerControls.Shoot.performed += OnShoot;
+    //    playerMovement.PlayerControls.Pass.performed += OnPass;
+    //    playerMovement.PlayerControls.Receive.performed += OnReceive;
+    //}
 
-    private void OnReceive(InputAction.CallbackContext context)
-    {
-        playerAnimator.SetTrigger("ReceiveBall");
-        ballAnimator.SetTrigger("ReceiveBall");
-        Debug.Log($"按下接球键K" + context);
-    }
+    //private void OnReceive(InputAction.CallbackContext context)
+    //{
+    //    playerAnimator.SetTrigger("ReceiveBall");
+    //    ballAnimator.SetTrigger("ReceiveBall");
+    //    Debug.Log($"按下接球键K" + context);
+    //}
 
-    private void OnPass(InputAction.CallbackContext context)
-    {
-        Debug.Log($"按下传球键K" + context);
-    }
+    //private void OnPass(InputAction.CallbackContext context)
+    //{
+    //    Debug.Log($"按下传球键K" + context);
+    //}
 
-    private void OnShoot(InputAction.CallbackContext context)
-    {
-        playerAnimator.SetBool("ShootBall",true);
-        ballAnimator.SetBool("ShootBall", true);
-        Debug.Log($"按下投篮键J" + context);
-    }
+    //private void OnShoot(InputAction.CallbackContext context)
+    //{
+    //    playerAnimator.SetBool("ShootBall",true);
+    //    ballAnimator.SetBool("ShootBall", true);
+    //    Debug.Log($"按下投篮键J" + context);
+    //}
 
-    private void OnMoveCanceled(InputAction.CallbackContext context)
-    {
-        move = Vector2.zero;
-        playerAnimator.SetFloat("MoveX", move.x);
-        playerAnimator.SetFloat("MoveY", move.y);
-        playerAnimator.SetBool("IsJogWithBall", false);
+    //private void OnMoveCanceled(InputAction.CallbackContext context)
+    //{
+    //    move = Vector2.zero;
+    //    playerAnimator.SetFloat("MoveX", move.x);
+    //    playerAnimator.SetFloat("MoveY", move.y);
+    //    playerAnimator.SetBool("IsJogWithBall", false);
 
-        ballAnimator.SetFloat("BallMoveX", move.x);
-        ballAnimator.SetFloat("BallMoveY", move.y);
-        ballAnimator.SetBool("IsJogWithBall", false);
+    //    ballAnimator.SetFloat("BallMoveX", move.x);
+    //    ballAnimator.SetFloat("BallMoveY", move.y);
+    //    ballAnimator.SetBool("IsJogWithBall", false);
 
-        Debug.Log($"<color=red>取消按下方向键{move}</color>" + context);
-    }
+    //    Debug.Log($"<color=red>取消按下方向键{move}</color>" + context);
+    //}
 
-    private void OnMove(InputAction.CallbackContext context)
-    {
-        playerAnimator.SetBool("IsJogWithBall",true);
-        ballAnimator.SetBool("IsJogWithBall", true);
-        move = context.ReadValue<Vector2>();
-        Debug.Log($"按下方向键{move}" + context);
-    }
+    //private void OnMove(InputAction.CallbackContext context)
+    //{
+    //    playerAnimator.SetBool("IsJogWithBall",true);
+    //    ballAnimator.SetBool("IsJogWithBall", true);
+    //    move = context.ReadValue<Vector2>();
+    //    Debug.Log($"按下方向键{move}" + context);
+    //}
 
-    private void OnEnable()
-    {
-        playerMovement.PlayerControls.Enable();
-    }
+    //private void OnEnable()
+    //{
+    //    playerMovement.PlayerControls.Enable();
+    //}
 
-    void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(move.x, 0.0f, move.y) * speed * Time.deltaTime;
-        playerAnimator.SetFloat("MoveX", move.x);
-        playerAnimator.SetFloat("MoveY", move.y);
+    //void FixedUpdate()
+    //{
+    //    Vector3 movement = new Vector3(move.x, 0.0f, move.y) * speed * Time.deltaTime;
+    //    playerAnimator.SetFloat("MoveX", move.x);
+    //    playerAnimator.SetFloat("MoveY", move.y);
 
-        ballAnimator.SetFloat("BallMoveX", move.x);
-        ballAnimator.SetFloat("BallMoveY", move.y);
-        transform.Translate(movement,Space.World);
-    }
+    //    ballAnimator.SetFloat("BallMoveX", move.x);
+    //    ballAnimator.SetFloat("BallMoveY", move.y);
+    //    transform.Translate(movement,Space.World);
+    //}
 
-    private void OnDisable()
-    {
-        playerMovement.PlayerControls.Disable();
-    }
+    //private void OnDisable()
+    //{
+    //    playerMovement.PlayerControls.Disable();
+    //}
 }
